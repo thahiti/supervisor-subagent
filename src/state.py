c6c75c2eaf4ea1917706130ab2f954d86ec2f466
randy <thahiti@gmail.com>
2026-04-28 09:45:13 +0900

    feat(state): add chat_history field with concat reducer
    호출자가 큐레이션한 과거 대화를 별도 필드로 관리하기 위해
    operator.add 리듀서를 사용하는 chat_history 필드를 추가한다.

--- src/state.py
+++ src/state.py
@@ -1,3 +1,4 @@
+import operator
 from typing import Annotated, TypedDict
 
 from langchain_core.messages import BaseMessage
@@ -8,12 +9,16 @@ class State(TypedDict):
     """Router-Subagent 시스템의 공유 상태.
 
     Attributes:
-        messages: 전체 대화 이력 (add_messages 리듀서로 자동 병합)
+        messages: 단일 invocation 내의 메시지 흐름 (add_messages 리듀서로 자동 병합)
         next_agent: 라우터가 결정한 다음 워커 (registry 이름 또는 "FINISH")
+        chat_history: 호출자가 큐레이션한 과거 대화. 한 턴 종료 시
+            (리라이팅된 사용자 질의 HumanMessage, 최종 출력 AIMessage) 2개가
+            operator.add 리듀서로 누적된다.
     """
 
     messages: Annotated[list[BaseMessage], add_messages]
     next_agent: str
+    chat_history: Annotated[list[BaseMessage], operator.add]
 
 
 class WorkerState(TypedDict):
